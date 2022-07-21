1692d60d17d1199536f8ca60c92eabbc5786165c
dtysky <dtysky@outlook.com>
2022-07-21 12:59:10 +0800

    Update demos

--- miniprogram/components/xr-custom-logic/index.js
+++ miniprogram/components/xr-custom-logic/index.js
@@ -6,19 +6,36 @@ Component({
   lifetimes: {},
   methods: {
     handleReady: function({detail}) {
-      const scene = detail.value;
+      this.scene = detail.value;
+      this.rotSpeed = 1;
+      this.rotAxis = 0;
     },
     handleTouchStart: function({detail}) {
-      const info = detail.value;
-      console.log('touch start', info)
+      console.log('touch start', detail.value);
     },
     handleTouchEnd: function({detail}) {
-      const info = detail.value;
-      console.log('touch end', info)
+      console.log('touch end', detail.value);
+      this.rotAxis += 1;
+      if (this.rotAxis >= 3) {
+        this.rotAxis = 0;
+      }
+
+      this.changeSpeed();
     },
     handleDrag: function({detail}) {
       const info = detail.value;
-      console.log('drag', info)
+      console.log('drag', info);
+      this.rotSpeed += info.deltaX / this.scene.width;
+      this.changeSpeed();
+    },
+    changeSpeed() {
+      const xrSystem = wx.getXrFrameSystem();
+      const el = this.scene.getElementById('artg');
+      const comp = el.getComponent('auto-rotate');
+      
+      if (comp) {
+        comp.setData({speed: [0, 0, 0].map((_, i) => i === this.rotAxis ? this.rotSpeed : 0)});
+      }
     },
     handleAssetsProgress: function({detail}) {
       console.log('assets progress', detail.value);
@@ -26,6 +43,9 @@ Component({
     handleAssetsLoaded: function({detail}) {
       console.log('assets loaded', detail.value);
       this.setData({loaded: true});
+    },
+    handleLog: function({detail}) {
+      console.log('log', detail.value);
     }
   }
 })
