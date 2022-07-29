24b3aaeb65eee753399104854c5f9cee330ebc45
dtysky <dtysky@outlook.com>
2022-07-29 14:14:31 +0800

    Update events

--- miniprogram/components/xr-scan-team/index.js
+++ miniprogram/components/xr-scan-team/index.js
@@ -16,6 +16,16 @@ Component({
     handleReady({detail}) {
       const xrScene = this.scene = detail.value;
       console.log('xr-scene', xrScene);
+      this.bgm = wx.createInnerAudioContext();
+      this.bgm.src = 'https://mmbizwxaminiprogram-1258344707.cos.ap-guangzhou.myqcloud.com/xr-frame/demo/kaqituolitai.mp3';
+      this.voice = wx.createInnerAudioContext({useWebAudioImplement: true});
+      this.voice.src = 'https://mmbizwxaminiprogram-1258344707.cos.ap-guangzhou.myqcloud.com/xr-frame/demo/homo.m4a';
+      this.voiceFrag = {
+        preXinyi: [4, 2],
+        preRoam: [6.5, 2],
+        preHikari: [48, 3],
+        jump: [53.5, 2]
+      };
     },
     handleAssetsLoaded: function({detail}) {
       this.triggerEvent('assetsLoaded', detail.value);
@@ -58,32 +68,39 @@ Component({
     run: async function() {
       this.cameraCtrl.disable();
 
-      await this.prepareRun(this.xinyi);
-      await this.prepareRun(this.roam);
-      await this.prepareRun(this.hikari);
+      await this.prepareRun(this.xinyi, this.voiceFrag.preXinyi);
+      await this.prepareRun(this.roam, this.voiceFrag.preRoam);
+      await this.prepareRun(this.hikari, this.voiceFrag.preHikari);
 
-      const trs = this.camera.el.getComponent(wx.getXrFrameSystem().Transform);
-      trs.position.setValue(0, 0.8, 3);
-      this.cameraTarget.position.setValue(0, 0.8, 0);
       this.runOne(this.hikari);
       this.runOne(this.roam);
       this.runOne(this.xinyi);
 
       this.cameraCtrl.enable();
     },
-    prepareRun: async function(char) {
+    prepareRun: async function(char, voiceFrag) {
       return new Promise(resolve => {
+        this.voice.seek(voiceFrag[0]);
+        this.voice.play();
         const trs = this.camera.el.getComponent(wx.getXrFrameSystem().Transform);
         trs.position.setValue(char.position.x, 0.8, 2);
         this.cameraTarget.position.setValue(char.position.x, 0.8, 0);
         const animator = char.el.getComponent(wx.getXrFrameSystem().Animator);
 
         setTimeout(() => {
+          this.voice.stop();
           animator.stop();
           animator.pauseToFrame('Run', 0);
           setTimeout(() => resolve(), 300);
-        }, 1000);
+        }, voiceFrag[1] * 1000);
       });
+    },
+    prepareCamera: async function() {
+      this.bgm.play();
+      this.bgm.seek(43);
+      const trs = this.camera.el.getComponent(wx.getXrFrameSystem().Transform);
+      trs.position.setValue(0, 0.8, 3);
+      this.cameraTarget.position.setValue(0, 0.8, 0);
     },
     runOne: function(char) {
       const animator = char.el.getComponent(wx.getXrFrameSystem().Animator);
