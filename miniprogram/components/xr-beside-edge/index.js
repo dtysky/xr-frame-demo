0a8b2f7e6d821bb773bf756f2bdc96115ee372c0
dtysky <dtysky@outlook.com>
2023-02-05 03:01:54 +0800

    Update beside edge

--- miniprogram/components/xr-beside-edge/index.js
+++ miniprogram/components/xr-beside-edge/index.js
@@ -3,24 +3,109 @@ const dpi = info.pixelRatio;
 const width = info.windowWidth * dpi;
 const height = info.windowHeight * dpi;
 
+import CONFIG from './config.js';
+
+const ROOT_DURATIONS = [
+  3000, 6000, 10000, Infinity
+];
+const ROOT_DELAYS = [
+  1000, 3000, 1000, 0
+];
+const ROOT_AMBIENTS = [
+  0.1, 0.3, 0.6, 1
+];
+
+function showFinalChoose(content, callback) {
+  wx.showModal({
+    title: '做出选择',
+    content,
+    showCancel: true,
+    confirmText: '正视现实',
+    cancelText: '我不要！',
+    success: (res) => {
+      if (res.cancel) {
+        showFinalChoose('不能逃避！', callback);
+      } else {
+        callback();
+        setTimeout(() => {
+          wx.showToast({
+            title: '勇敢一些\n不要怯懦',
+            duration: 2000,
+            icon: 'none'
+          })
+        }, 500);
+      }
+    }
+  });
+}
+
 Component({
   scene: null,
-  properties: {},
+  properties: {
+    nextAction: {
+      type: String,
+      value: '',
+      observer(fromWhat, old) {
+        if (fromWhat === 'item') {
+          this.remainItems -= 1;
+          if (this.remainItems === 0) {
+            this.disable3DTouch = false;
+            this.setData({subStep: true});
+            return;
+          }
+
+          this.switchSide(false);
+          this.triggerEvent('requireLight', {state: 'idle'});
+          return;
+        }
+
+        if (fromWhat === 'char') {
+          const {texts} = this.config.steps[this.data.step];
+          this.requireDialog({texts, from: 'step'});
+          return;
+        }
+
+        if (fromWhat === 'step' || fromWhat === 'intro') {
+          const step = this.data.step + 1;
+          if (step === 4) {
+            wx.showModal({
+              title: '最后的信',
+              content: this.config.letter.texts.join('\n'),
+              showCancel: false,
+              confirmText: '做出选择',
+              complete: () => {
+                showFinalChoose('', () => this.switchSide(false));
+              }
+            });
+            return;
+          }
+
+          this.remainItems = this.config.steps[step].itemCount;
+          this.setData({step, subStep: false, ambient: ROOT_AMBIENTS[step]});
+          this.switchSide(false);
+          this.triggerEvent('requireLight', {state: 'idle'});
+          return;
+        }
+
+        if (fromWhat === 'light') {
+          this.switchSide(true);
+          this.disable3DTouch = false;
+          this.lightDuration = ROOT_DURATIONS[this.data.step];
+          this.lightDelay = ROOT_DELAYS[this.data.step];
+        }
+      }
+    }
+  },
   data: {
+    step: -1,
+    subStep: false,
+    ambient: 0,
     width: width,
     height: height,
     loaded: false,
-    arReady: false,
-    placed: false,
-    gateClosed: false
+    arReady: false
   },
   lifetimes: {
-    attached() {
-      wx.reportEvent("xr_frame", {
-        "xr_page_path": '/pages/scene-last-record/index',
-        "xr_last_record_click": 1
-      });
-    },
     detached() {
       this.bgm.stop();
       wx.setKeepScreenOn({keepScreenOn: false});
@@ -30,194 +115,116 @@ Component({
     handleReady({detail}) {
       this.scene = detail.value;
       this.scene.event.add('tick', this.handleTick.bind(this));
-      this.inRealWorld = true;
-      this.texts = {};
-      this.textsIndex = {};
+      this.inited = false;
+      this.disable3DTouch = false;
+      this.remainItems = 0;
+      this.lightDuration = 0;
+      this.lightDelay = 0;
       this.bgm = wx.createInnerAudioContext({});
       this.bgm.src = 'https://mmbizwxaminiprogram-1258344707.cos.ap-guangzhou.myqcloud.com/xr-frame/demo/xr-frame-team/bgm.mp3';
       this.bgm.loop = true;
     },
-    handleAssetsProgress: function ({detail}) {
-      console.log('assets progress', detail.value);
-    },
     handleARReady() {
+      wx.setKeepScreenOn({keepScreenOn: true});
       this.setData({arReady: true});
     },
     handleAssetsLoaded: function ({detail}) {
       console.log('assets loaded', detail.value);
-      this.records = JSON.parse(this.scene.assets.getAsset('raw', 'records'));
+      this.config = CONFIG;
       this.note = this.scene.assets.getAsset('raw', 'note');
       this.setData({loaded: true});
     },
     handleTick(dt) {
-      this.syncTexts();
-
-      if (!this.data.placed || !this.inRealWorld) {
-        return;
-      }
-
-      const xrSystem = wx.getXrFrameSystem();
-      const mainCamEl = this.scene.getElementById('main-camera');
-      const mainTrs = mainCamEl.getComponent(xrSystem.Transform);
-      const door = this.scene.getElementById('door').getComponent(xrSystem.Transform);
-
-      let forward = door.worldForward;
-      forward = xrSystem.Vector2.createFromNumber(forward.x, forward.z);
-      let diff = mainTrs.worldPosition.sub(door.worldPosition);
-      diff = xrSystem.Vector2.createFromNumber(diff.x, diff.z);
-      const preDiff = this.diff || diff;
-      this.diff = diff;
-
-      const dis = diff.length();
-      const preDis = preDiff.length();
-      const dir = forward.dot(diff);
-      this.startDis = this.startDis || dis;
-
-      const blurAsset = this.scene.assets.getAsset('post-process', 'blur');
-      const vignetteAsset = this.scene.assets.getAsset('post-process', 'vignette');
-      const bloomAsset = this.scene.assets.getAsset('post-process', 'bloom');
-      const edgeEnv1 = 0.5;
-      const edgeEnv2 = 0.8;
-      const edgeDoor1 = 0.3;
-      const edgeDoor2 = 0.7;
-
-      if (this.blurDuration) {
-        this.blurDuration = Math.max(0, this.blurDuration - dt);
-        const p = 1 - this.blurDuration / this.blurTotal;
-
-        if (p <= edgeEnv1) {
-          const progress = xrSystem.noneParamsEaseFuncs['ease-in-out'](p / edgeEnv1);
-          vignetteAsset.data.intensity = progress * 2;
-          blurAsset.data.radius = progress * 86 + 10;
-        } else if (p > edgeEnv2) {
-          const progress = xrSystem.noneParamsEaseFuncs['ease-in-out']((1 - p) / (1 - edgeEnv2));
-          vignetteAsset.data.intensity = progress * 2;
-          blurAsset.data.radius = progress * 96;
+      if (this.inited) {
+        const nextDuration = Math.max(this.lightDuration - dt, 0);
+        if (this.lightDuration > 0 && nextDuration === 0) {
+          this.disable3DTouch = true;
+          this.switchSide(false);
+          this.triggerEvent('requireLight', {state: 'cd', wait: 1});
+        } else if (this.lightDuration === 0) {
+          const nextDelay = Math.max(this.lightDelay - dt, 0);
+          if (this.lightDelay > 0 && nextDelay === 0) {
+            this.triggerEvent('requireLight', {state: 'idle'});
+          } else if (this.lightDelay > 0) {
+            this.triggerEvent('requireLight', {state: 'cd', wait: nextDelay / ROOT_DELAYS[this.data.step]});
+          }
+          this.lightDelay = nextDelay;
         }
-        
-        if (p >= edgeDoor1 && p < edgeDoor2) {
-          const progress = xrSystem.noneParamsEaseFuncs['ease-in-out']((p - edgeDoor1) / (edgeDoor2 - edgeDoor1));
-          door.scale.setValue(progress, 1, 1);
-        }
-      } else if (this.blurTotal) {
-        let progress = (1 - Math.max(0, Math.min(dis / this.startDis, 0.8)));
-        if (progress >= 0.2) {
-          progress = (progress - 0.2) / 0.6;
-          blurAsset.data.radius = progress * 96;
-          vignetteAsset.data.intensity = progress * 2;
-          bloomAsset.data.threshold = 0.5 + progress * 2;
-        }
-      }
 
-      //@todo: 等待物理加上碰撞检测，替换
-      if (dir >= 0 || preDis <= 0.2 || dis > 0.2) {
+        this.lightDuration = nextDuration;
         return;
       }
 
-      ['sky', 'scene-mesh', 'hikari', 'roam', 'xinyi'].forEach(id => {
-        this.scene
-          .getElementById(id)
-          .getComponent(xrSystem.GLTF).meshes.forEach(mesh => mesh.material.setRenderState('stencilComp', 0));
-      });
-      mainCamEl.getComponent(xrSystem.Camera).setData({
-        renderTarget: null,
-        postProcess: ['tone']
-      });
-      this.setData({gateClosed: true});
-      this.inRealWorld = false;
-    },
-    handleShowDoor({detail}) {
-      if (detail.value.camera.el.id !== 'main-camera') {
+      if (!this.data.arReady || !this.data.loaded) {
         return;
       }
 
-      const success = this.scene.ar.placeHere('setitem', true);
-      if (!success) {
-        return;
-      }
+      const mainCamera = this.scene.getNodeById('main-camera');
+      const setItem = this.scene.getNodeById('setitem');
+      setItem.position.set(mainCamera.position);
+      setItem.position.y = 1.5;
+      this.inited = true;
 
       setTimeout(() => {
-        this.blurTotal = this.blurDuration = 1700;
-      }, 300);
-      wx.setKeepScreenOn({keepScreenOn: true});
-      this.bgm.play();
-      this.setData({placed: true});
+        this.switchSide(true);
+        setTimeout(() => {
+          const {texts} = this.config.intro;
+          this.requireDialog({texts, from: 'intro'});
+          this.bgm.play();
+        }, 500);
+      }, 1000);
+    },
+    switchSide(virtual) {
+      const setItem = this.scene.getNodeById('setitem');
+      setItem.visible = virtual;
+      virtual && this.triggerEvent('requireLight', {state: 'hide'});
     },
     handleResume() {
       if (this.data.placed) {
         this.bgm.play();
       }
     },
-    handleTouchNote({detail}) {
-      if (detail.value.camera.el.id !== 'main-camera') {
+    handleTouchObj({detail}) {
+      if (!this.inDistance(detail)) {
         return;
       }
 
-      this.triggerEvent('showNote', this.note);
+      const id = detail.value.target.id;
+      const {texts} = this.config.items[id];
+
+      this.requireDialog({texts, from: 'item'});
     },
-    handleTouchObj({detail}) {
-      if (detail.value.camera.el.id !== 'main-camera') {
+    handleTouchChar({detail}) {
+      if (!this.inDistance(detail)) {
         return;
+      }
+
+      const id = this.data.step === 3 ? 'final' : detail.value.target.id;
+      const {name, texts} = this.config.chars[id];
+      this.requireDialog({name, texts, from: 'char'});
+    },
+    requireDialog(info) {
+      this.disable3DTouch = true;
+      this.lightDuration = Infinity;
+      this.triggerEvent('requireLight', {state: 'hide'});
+      this.triggerEvent('requireDialog', info);
+    },
+    inDistance(detail) {
+      if (detail.value.camera.el.id !== 'main-camera') {
+        return false;
+      }
+
+      if (this.disable3DTouch) {
+        return false;
       }
 
       const xrSystem = wx.getXrFrameSystem();
-      const {el, value} = detail;
-      const {camera, target} = value;
-      const id = target.id;
-      let text = this.texts[id];
+      const {camera, target} = detail.value;
       const camTrs = camera.el.getComponent(xrSystem.Transform);
       const targetTrs = target.getComponent(xrSystem.Transform);
       const diff = camTrs.worldPosition.sub(targetTrs.worldPosition);
-      const distance = Math.sqrt(diff.x * diff.x + diff.z * diff.z);
 
-      if (!this.records[id]) {
-        return;
-      }
-
-      const {y, d, texts: records} = this.records[id];
-
-      if (distance > (d || 1.5)) {
-        return;
-      }
-
-      if (text) {
-        clearTimeout(text.timerId);
-      }
-
-      let index = this.textsIndex[id] === undefined ? -1 : this.textsIndex[id];
-      if (index >= records.length - 1) {
-        index = 0;
-      } else {
-        index += 1;
-      }
-      this.textsIndex[id] = index;
-
-      this.texts[id] = {
-        content: records[index],
-        camera, target, y,
-        timerId: setTimeout(() => {
-          delete this.texts[id];
-        }, 4000)
-      };
-    },
-    syncTexts: function() {
-      const texts = Object.keys(this.texts).map(id => {
-        const {camera, target, content, y} = this.texts[id];
-        const xrSystem = wx.getXrFrameSystem();
-        const trs = target.getComponent(xrSystem.Transform);
-        const tmp = trs.worldPosition.clone();
-        tmp.y += y;
-        const clipPos = camera.convertWorldPositionToClip(tmp);
-        const {frameWidth, frameHeight} = this.scene;
-  
-        return {
-          content, id,
-          x: ((clipPos.x + 1) / 2) * frameWidth,
-          y: (1 - (clipPos.y + 1) / 2) * frameHeight
-        };
-      });
-
-      this.triggerEvent('changeTexts', texts);
+      return Math.sqrt(diff.x * diff.x + diff.z * diff.z) < 1.5;
     }
   }
 })
--- miniprogram/pages/scene-beside-edge/index.js
+++ miniprogram/pages/scene-beside-edge/index.js
@@ -2,7 +2,9 @@ Page({
   data: {
     width: 300, height: 300,
     renderWidth: 300, renderHeight: 300,
-    texts: []
+    showDialog: false, name: '', text: '', bg: 'rgba(0, 0, 0, 0)',
+    showLightButton: false, lightButtonDisable: true,
+    nextAction: ''
   },
   onShow() {
     wx.hideHomeButton();
@@ -21,15 +23,53 @@ Page({
       renderHeight: height * dpi
     });
   },
-  handleChangeTexts: function({detail}) {
-    this.setData({texts: detail});
-  },
-  handleShowNote: function({detail}) {
-    wx.showModal({
-      title: '最后的记录',
-      content: detail,
-      showCancel: false,
-      confirmText: '放下记录'
+  handleRequireLight: function({detail}) {
+    const {state, wait} = detail;
+    console.log('light', state, wait)
+    if (state === 'hide') {
+      this.setData({showLightButton: false});
+      return;
+    }
+
+    this.setData({
+      showLightButton: true,
+      lightButtonDisable: state === 'cd',
+      nextAction: ''
+      // 处理cd进度
     });
+  },
+  handleRequireDialog: function({detail}) {
+    const {texts, name, from} = detail;
+    this.texts = texts;
+    this.textIndex = 0;
+    this.from = from;
+    this.setData({
+      name: name || '', text: texts[0], showDialog: true,
+      bg: from === 'step' ? '#000' : 'rgba(0, 0, 0, 0)'
+    });
+  },
+  handleClickDialog: function() {
+    this.textIndex += 1;
+    if (this.textIndex > this.texts.length) {
+      return;
+    }
+
+    if (this.textIndex === this.texts.length) {
+      this.setData({text: '', name: '', bg: '#000'});
+      setTimeout(() => {
+        this.textIndex = 0;
+        this.setData({showDialog: false, nextAction: this.from});
+      }, 1000);
+      return;
+    }
+
+    this.setData({text: this.texts[this.textIndex]});
+  },
+  handleTriggerLight: function() {
+    if (this.data.lightButtonDisable) {
+      return;
+    }
+
+    this.setData({nextAction: 'light'});
   }
 })
