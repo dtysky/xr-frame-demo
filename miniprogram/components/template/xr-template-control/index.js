5ebcc2ef8d3888a59b304716c74a257f7fb1fe9f
VirtualStarry <595158846@qq.com>
2023-03-03 19:55:08 +0800

    feat(fps-controller): 第一人称控制器

--- miniprogram/components/template/xr-template-control/index.js
+++ miniprogram/components/template/xr-template-control/index.js
@@ -1,23 +1,153 @@
+let camera;
+let player;
+let xrFrameSystem;
+// 位移速度
+let speed = 5;
+// 视角旋转的速度
+let smoothSpeed = 8;
+
+// 世界坐标系下的标准方位
+let up;
+let left;
+
+let renderWidth = 0;
+let renderHeight = 0;
+
+// player相关
+let position;
+let quaternionP; //player的四元数
+let quaternionPIni; //player每次转动开始时的角度
+let quaternionPRes; //player每次需要转动到的角度
+
+// camera相关
+let quaternionC; //camera的四元数
+let quaternionCIni;  //camera每次转动开始时的角度
+let quaternionCRes;  //camera每次需要转动到的角度
+
+// 初始化是否完成的标记
+let initFinish = false;
+
 Component({
   behaviors: [require('../../common/share-behavior').default],
   properties: {
-    a: Number,
+    touchX: Number,
+    touchY: Number,
+    radius: Number,
+    rotateX: Number,
+    rotateY: Number,
+    initX: Number,
+    initY: Number,
+    reset: {
+      type: Number,
+      observer(newVal) {
+        position.set(xrFrameSystem.Vector3.createFromNumber(0, 1.6, 1));
+        quaternionC.setFromYawRollPitch(-Math.PI, 0, 0);
+        quaternionP.setFromYawRollPitch(0, 0, 0);
+      }
+    }
   },
   data: {
     loaded: false
   },
   lifetimes: {},
   methods: {
-    handleReady({detail}) {
+    handleReady({
+      detail
+    }) {
       const xrScene = this.scene = detail.value;
       console.log('xr-scene', xrScene);
+      xrFrameSystem = wx.getXrFrameSystem();
+      camera = xrScene.getElementById("camera");
+      player = xrScene.getElementById("player");
+      xrScene.event.add('touchmove', this.handleTouchMove);
+      xrScene.event.add('touchend', this.handleTouchEnd);
+
+      up = xrFrameSystem.Vector3.createFromNumber(0, 1, 0);
+      left = xrFrameSystem.Vector3.createFromNumber(1, 0, 0);
+
+      const info = wx.getSystemInfoSync();
+      const width = info.windowWidth;
+      const windowHeight = info.windowHeight;
+      const height = windowHeight * 0.75;
+      const dpi = info.pixelRatio;
+      renderWidth = width * dpi;
+      renderHeight = height * dpi;
+
+      quaternionC = camera._components.transform.quaternion;
+      quaternionP = player._components.transform.quaternion;
+      position = player._components.transform.position;
+
+      quaternionPIni = new xrFrameSystem.Quaternion();
+      quaternionPIni.set(quaternionP);
+      quaternionPRes = new xrFrameSystem.Quaternion();
+      quaternionPRes.set(quaternionP);
+
+      quaternionCIni = new xrFrameSystem.Quaternion();
+      quaternionCIni.set(quaternionC);
+      quaternionCRes = new xrFrameSystem.Quaternion();
+      quaternionCRes.set(quaternionC);
+
+      initFinish = true;
+
     },
-    handleAssetsProgress: function({detail}) {
+    handleAssetsProgress: function ({
+      detail
+    }) {
       console.log('assets progress', detail.value);
     },
-    handleAssetsLoaded: function({detail}) {
+    handleAssetsLoaded: function ({
+      detail
+    }) {
       console.log('assets loaded', detail.value);
-      this.setData({loaded: true});
-    }
+      this.setData({
+        loaded: true
+      });
+    },
+    handleTick: function (dt) {
+
+      //确保handleReady时期的初始化完成
+      if (!initFinish)
+        return;
+
+      var deltaTime = dt.detail.value / 1000;
+
+      //------摄像头旋转逻辑------//
+      let rotX = (this.data.rotateX - this.data.initX) / renderWidth * Math.PI;
+      let rotY = (this.data.rotateY - this.data.initY) / renderHeight * Math.PI;
+
+      //水平方向旋转player node
+      if (this.data.rotateX == 0) {
+        quaternionPIni.set(quaternionP);
+        quaternionPRes.set(quaternionP);
+      } else {
+        quaternionPIni.multiply(xrFrameSystem.Quaternion.createFromAxisAngle(up, -rotX), quaternionPRes);
+      }
+
+      //垂直方向旋转camera node
+      if (this.data.rotateY == 0) {
+        quaternionCIni.set(quaternionC);
+        quaternionCRes.set(quaternionC);
+      } else {
+        quaternionCIni.multiply(xrFrameSystem.Quaternion.createFromAxisAngle(left, rotY), quaternionCRes);
+      }
+
+      quaternionP.slerp(quaternionPRes, smoothSpeed * deltaTime, quaternionP);
+      quaternionC.slerp(quaternionCRes, smoothSpeed * deltaTime, quaternionC);
+
+       //------摄像头位移逻辑------//
+      var x = this.data.touchX;
+      var y = this.data.touchY;
+
+      if (x || y) {
+        var z = Math.sqrt(x * x + y * y);
+        var ratio = z / this.data.radius;
+        ratio = ratio > 1 ? 1 : ratio < 0 ? 0 : ratio;
+        var temp = xrFrameSystem.Vector3.createFromNumber(-x / z, 0, -y / z);
+        temp = temp.scale(ratio * speed * deltaTime);
+        //位移需要根据旋转角度做转化
+        temp.applyQuaternion(quaternionP);
+        position.set(position.add(temp));
+      }
+    },
   }
 })
--- miniprogram/pages/template/xr-template-control/index.js
+++ miniprogram/pages/template/xr-template-control/index.js
@@ -22,9 +22,139 @@ var xmlCode = `&lt;xr-scene id=&quot;xr-scene&quot;&gt;
 &lt;/xr-node&gt;
 &lt;/xr-scene&gt;`;
 
+// 当前触摸点位置
+let currentTouchX = 0;
+let currentTouchY = 0;
+
+// 触摸点序号
+let rotateIdx = -1;
+let moveIdx = -1;
+
 Page({
-  behaviors:[sceneReadyBehavior],
+  behaviors: [sceneReadyBehavior],
   data: {
     xmlCode: '<div class="codeWrap">' + handleDecodedXML(xmlCode) + '</div>',
+    //摇杆盘半径
+    radius: 50,
+    //-----摇杆对应属性-----//
+    //所处中心
+    centerX: 50,
+    centerY: 50,
+    //相对背景偏移，命名 h=handle 摇杆
+    hTop: 30,
+    hLeft: 30,
+    //摇杆宽高
+    hWidth: 40,
+    hHeight: 40,
+    //重置初始位置的标记
+    reset: 0,
+  },
+  reset: function () {
+    this.setData({
+      reset: this.data.reset + 1
+    })
+  },
+  debugShow: function (functionName, moveIdx, rotateIdx) {
+    // console.log(functionName+","+moveIdx+","+rotateIdx);
+  },
+  touchStart(e) {},
+  touchMove: function (e) {
+     //无旋转触摸点触屏时，保证移动触摸点为第一触摸点
+    if (rotateIdx == -1) {
+      moveIdx = 0;
+    } else {
+      //当两点触屏时，保证移动触摸点与旋转触摸点对应的点序列互斥
+      moveIdx = ~rotateIdx & 0x01;
+    }
+    this.debugShow("touchMove", moveIdx, rotateIdx);
+    var touchX = e.touches[moveIdx].clientX - 30;
+    var touchY = e.touches[moveIdx].clientY - (this.data.height - 100);
+    var posInfo = this.limitPosition(touchX, touchY);
+    this.setData({
+      biasX: posInfo.posX,
+      biasY: posInfo.posY,
+      hLeft: this.data.centerX - this.data.hWidth / 2 + posInfo.posX,
+      hTop: this.data.centerY - this.data.hHeight / 2 + posInfo.posY
+    })
+  },
+  touchEnd: function (e) {
+    moveIdx = -1;
+    //当位移触摸点松开时，保证旋转触摸点取的为第一接触点
+    if (rotateIdx == 1)
+      rotateIdx = 0;
+    this.setData({
+      biasX: 0,
+      biasY: 0,
+      hLeft: this.data.centerX - this.data.hWidth / 2,
+      hTop: this.data.centerY - this.data.hHeight / 2
+    });
+  },
+
+  touchRotateStart: function (e) {
+    if (moveIdx == -1) {
+      rotateIdx = 0;
+    } else {
+      rotateIdx = ~moveIdx & 0x01;
+    }
+    this.debugShow("touchRotateStart", moveIdx, rotateIdx);
+    currentTouchX = e.touches[rotateIdx].clientX;
+    currentTouchY = e.touches[rotateIdx].clientY;
+    this.setData({
+      initX: currentTouchX,
+      initY: currentTouchY,
+    })
+  },
+
+  touchRotateMove: function (e) {
+    if (moveIdx == -1) {
+      rotateIdx = 0;
+    } else {
+      rotateIdx = ~moveIdx & 0x01;
+    }
+    this.debugShow("touchRotateMove", moveIdx, rotateIdx);
+    currentTouchX = e.touches[rotateIdx].clientX;
+    currentTouchY = e.touches[rotateIdx].clientY;
+    this.setData({
+      rotateX: currentTouchX,
+      rotateY: currentTouchY,
+    })
+  },
+
+  touchRotateEnd: function (e) {
+    rotateIdx = -1;
+    //当旋转触摸点松开时，保证位移触摸点取的为第一接触点
+    if (moveIdx == 1)
+      moveIdx = 0;
+    this.setData({
+      rotateX: 0,
+      rotateY: 0,
+    })
+  },
+
+  // 将位移强度限制在摇杆盘的范围中
+  limitPosition: function (touchX, touchY) {
+    var x = touchX - this.data.centerX;
+    var y = touchY - this.data.centerY;
+    var z = Math.sqrt(x * x + y * y);
+    // 位移未超出摇杆盘范围时
+    if (z <= this.data.radius) {
+      x = Math.round(x);
+      y = Math.round(y);
+      return {
+        posX: x,
+        posY: y
+      };
+    } else {
+      // 位移超出摇杆盘范围，需要对应限制位移强度
+      var ratio = this.data.radius / z;
+      x = x * ratio;
+      y = y * ratio;
+      x = Math.round(x);
+      y = Math.round(y);
+      return {
+        posX: x,
+        posY: y
+      };
+    }
   }
 });
