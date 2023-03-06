e7a5331340d7b1bcdc4b4994e5cce72b9a894761
Ye Zhen Peng <roamzhen@gmail.com>
2023-03-06 21:30:13 +0800

    提交一版基础版本的多tracker

--- miniprogram/components/template/xr-template-tracker/index.js
+++ miniprogram/components/template/xr-template-tracker/index.js
@@ -1,10 +1,24 @@
 Component({
   behaviors: [require('../../common/share-behavior').default],
   properties: {
-    a: Number,
+    markerListRaw: {
+      type: Array,
+      value: []
+    },
   },
+  
   data: {
-    loaded: false
+    loaded: true,
+    arReady: false,
+  },
+  observers: {
+    markerListRaw(newVal) {
+      this.setData({
+        markerList: newVal,
+      })
+
+      console.log(this.data.markerList)
+    },
   },
   lifetimes: {},
   methods: {
@@ -12,12 +26,46 @@ Component({
       const xrScene = this.scene = detail.value;
       console.log('xr-scene', xrScene);
     },
-    handleAssetsProgress: function({detail}) {
-      console.log('assets progress', detail.value);
+    handleARReady: function() {
+      console.log('arReady')
+      this.setData({ arReady: true })
     },
-    handleAssetsLoaded: function({detail}) {
-      console.log('assets loaded', detail.value);
-      this.setData({loaded: true});
-    }
+    handleTrackerSwitch({ detail }) {
+      console.log('tracked match')
+
+      if (this.data.loaded) {
+        const element = detail.el;
+        const active = detail.value
+
+        const data = this.data
+        const markerList = data.markerList
+        for (let i = 0; i < markerList.length; i++) {
+          const markerInfo = markerList[i]
+          const markerTracker = this.scene.getElementById(`marker-${markerInfo.id}`)
+          if (element === markerTracker) {
+            // 匹配 tracker
+            const renderType = markerInfo.renderType
+            const xrFrameSystem = wx.getXrFrameSystem()
+            const camera = this.scene.getElementById('camera').getComponent(xrFrameSystem.Camera);
+            const trackerTRS = markerTracker.getComponent(xrFrameSystem.Transform)
+
+            switch (renderType) {
+              case 'scan': // scan
+                break;
+              case 'video': // video
+                break;
+              case 'gltf': // gltf
+                break;
+            }
+
+            this.triggerEvent('trackerchange', {
+              name: markerInfo.name,
+              active: active,
+              type: renderType,
+            })
+          }
+        }
+      }
+    },
   }
 })
