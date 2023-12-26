87b2caa1a83a168a74c6569a3953a48d2a029493
Ye Zhen Peng <roamzhen@gmail.com>
2023-12-26 14:39:56 +0800

    Update index.js

--- miniprogram/pages/ar/scene-ar-2dmarker/index.js
+++ miniprogram/pages/ar/scene-ar-2dmarker/index.js
@@ -25,8 +25,7 @@ Page({
   behaviors: [sceneReadyBehavior],
   data: {
     xmlCode: '<div class="codeWrap">' + handleDecodedXML(xmlCode) + '</div>',
-    // markerImg: 'https://mmbizwxaminiprogram-1258344707.cos.ap-guangzhou.myqcloud.com/xr-frame/demo/marker/2dmarker-test.jpg'
-    markerImg:'http://10.9.169.136:8000/Shishimodel.map',
+    markerImg: 'https://mmbizwxaminiprogram-1258344707.cos.ap-guangzhou.myqcloud.com/xr-frame/demo/marker/2dmarker-test.jpg'
   },
   handleChangeMarkerImg: function() {
     wx.chooseMedia({
